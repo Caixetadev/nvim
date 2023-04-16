75dcfe6d5539505a936905a37f982bf2af08e236
Rafael Caixeta <caixetadev@gmail.com>
2023-04-16 03:14:43 -0300

    feat: add config for treesitter

--- init.lua
+++ init.lua
@@ -151,3 +151,27 @@ vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true })
 
 -- move-se para a janela à direita
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true })
+
+-- import nvim-treesitter plugin safely
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status then
+	return
+end
+
+-- configure treesitter
+treesitter.setup({
+	-- enable syntax highlighting
+	highlight = {
+		enable = true,
+	},
+	-- enable indentation
+	indent = { enable = true },
+	-- enable autotagging (w/ nvim-ts-autotag plugin)
+	autotag = { enable = true },
+	-- ensure these language parsers are installed
+	ensure_installed = "all",
+	-- auto install above language parsers
+	auto_install = true,
+	-- magic
+	sync_install = false,
+})
