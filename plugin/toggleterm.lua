50af65efe06915e665616649d7c391400dd07886
Rafael Caixeta <caixetadev@gmail.com>
2023-05-06 02:08:57 -0300

    refactor: change config

--- plugin/toggleterm.lua
+++ plugin/toggleterm.lua
@@ -1,19 +1,31 @@
-require("toggleterm").setup{
-  size = 80,
-  direction = 'vertical' ,
-  open_mapping = [[\]],
-}
+require("toggleterm").setup({
+	size = function(term)
+		if term.direction == "horizontal" then
+			return 15
+		elseif term.direction == "vertical" then
+			return 80
+		end
+	end,
+})
 
-function _G.set_terminal_keymaps()
-  local opts = {noremap = true, silent = true}
+local Terminal = require('toggleterm.terminal').Terminal
+local right_term = Terminal:new({
+	direction = 'vertical',
+	on_open = function()
+		vim.cmd('startinsert!')
+		local opts = { buffer = 0 }
+		vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+		vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+		vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+		vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+		vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+		vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+		vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+	end
+})
 
-  -- Define os mesmos atalhos da configuração anterior
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+function RightTermToggle()
+	right_term:toggle()
 end
 
--- Define os atalhos quando um terminal é aberto
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.keymap.set('n', '\\', [[<Cmd>lua RightTermToggle()<CR>]], { noremap = true, silent = true })
