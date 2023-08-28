da4334bb1faf269e83cc05a42572106807424809
Rafael Caixeta <caixetadev@gmail.com>
2023-08-28 18:29:19 +0200

    feat: add harpoon

--- lua/caixeta/packer.lua
+++ lua/caixeta/packer.lua
@@ -67,33 +67,33 @@ return require('packer').startup(function(use)
     end
   }
 
-  -- use {
-  --   "olexsmir/gopher.nvim",
-  --   requires = {
-  --     "leoluz/nvim-dap-go"
-  --   },
-  --   config = function()
-  --     local gopher = require("gopher")
-  --     gopher.setup({
-  --       commands = {
-  --         go = "go",
-  --         gomodifytags = "gomodifytags",
-  --         gotests = "gotests",
-  --         impl = "impl",
-  --         iferr = "iferr",
-  --       },
-  --       goimport = "gopls",
-  --       gofmt = "gopls",
-  --     })
-  --   end,
-  --   -- Optional: disable lazy loading
-  --   -- and force the plugin to load immediately
-  --   -- when Neovim starts up.
-  --   -- However, keep in mind that this can slow down
-  --   -- Neovim's startup time, especially if you have many plugins.
-  --   -- Uncomment the following line to disable lazy loading.
-  --   -- cmd = "packadd! gopher.nvim",
-  -- }
+  use {
+    "olexsmir/gopher.nvim",
+    requires = {
+      "leoluz/nvim-dap-go"
+    },
+    config = function()
+      local gopher = require("gopher")
+      gopher.setup({
+        commands = {
+          go = "go",
+          gomodifytags = "gomodifytags",
+          gotests = "gotests",
+          impl = "impl",
+          iferr = "iferr",
+        },
+        goimport = "gopls",
+        gofmt = "gopls",
+      })
+    end,
+    -- Optional: disable lazy loading
+    -- and force the plugin to load immediately
+    -- when Neovim starts up.
+    -- However, keep in mind that this can slow down
+    -- Neovim's startup time, especially if you have many plugins.
+    -- Uncomment the following line to disable lazy loading.
+    -- cmd = "packadd! gopher.nvim",
+  }
 
   use 'kdheepak/lazygit.nvim'
 
@@ -155,6 +155,8 @@ return require('packer').startup(function(use)
   }
 
   use 'sheerun/vim-polyglot'
+
+  use 'ThePrimeagen/harpoon'
 
   use({
     "jose-elias-alvarez/null-ls.nvim",
