04b24d646133221993009e7d8003346fae9d9eba
Rafael Caixeta <caixetadev@gmail.com>
2023-09-01 14:54:46 +0200

    refactor: change keymap

--- lua/caixeta/remap.lua
+++ lua/caixeta/remap.lua
@@ -3,7 +3,6 @@ vim.g.mapleader = " "
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true })
-vim.api.nvim_set_keymap('i', 'kj', '<ESC>', { noremap = true })
+vim.api.nvim_set_keymap('i', 'jj', '<ESC>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true })
