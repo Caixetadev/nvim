5586a3a699502e1680c96d24fe940d859cf75821
Rafael Caixeta <caixetadev@gmail.com>
2023-09-15 00:26:19 +0200

    feat: update

--- lua/plugins/no_config.lua
+++ lua/plugins/no_config.lua
@@ -1,38 +1,7 @@
 return {
   "windwp/nvim-ts-autotag",
   "wakatime/vim-wakatime",
-
-  -- {
-  --   "echasnovski/mini.indentscope",
-  --   config = function ()
-  --     require("mini.indentscope").setup {
-  --       symbol = "│",
-  --       options = { try_as_border = true },
-  --     }
-  --   end
-  -- },
-
-  -- {
-  --   "mcchrish/zenbones.nvim",
-  --   -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-  --   -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-  --   -- In Vim, compat mode is turned on as Lush only works in Neovim.
-  --   dependencies = "rktjmp/lush.nvim",
-  --   config = function()
-  --     vim.cmd([[colorscheme zenbones]])
-  --   end,
-  -- },
-
-  -- Tema
-  -- use {'sainnhe/gruvbox-material',
-  --   config = function ()
-  --     vim.cmd('colorscheme gruvbox-material')
-  --     vim.g.gruvbox_material_background = "hard"
-  --     vim.opt.background = 'dark'
-  --   end
-  -- }
-
-  -- use 'andweeb/presence.nvim'
+  -- "andweeb/presence.nvim",
 
   -- 'ThePrimeagen/vim-be-good',
 
