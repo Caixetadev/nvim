810323082f249a721890615e453a0e51cd818004
Rafael Caixeta <caixetadev@gmail.com>
2023-09-15 21:43:45 +0200

    feat: add scrolloff

--- lua/caixeta/set.lua
+++ lua/caixeta/set.lua
@@ -28,6 +28,8 @@ vim.opt.updatetime = 100 -- faster completion (4000ms default)
 vim.opt.cursorline = true -- highlight the current line
 vim.opt.pumheight = 10 -- pop up menu height
 vim.opt.fillchars = { eob = " " } -- change the character at the end of buffer
+vim.opt.scrolloff = 8 -- Makes sure there are always eight lines of context
+vim.opt.sidescrolloff = 8 -- Makes sure there are always eight lines of context
 
 -- Buffers
 vim.api.nvim_set_keymap("n", "<leader>bd", ":bd<CR>", { noremap = true, silent = true, desc = "Close buffer" })
