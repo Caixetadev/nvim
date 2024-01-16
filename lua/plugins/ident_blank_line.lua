f87bf2f3b2ed4fb9159283ece5e93a05ddc7fddf
Rafael Caixeta <caixetadev@gmail.com>
2024-01-16 13:34:59 +0100

    refactor: change ident blank line

--- lua/plugins/ident_blank_line.lua
+++ lua/plugins/ident_blank_line.lua
@@ -1,12 +1,29 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufEnter" },
+  main = "ibl",
   opts = {
-    -- char = "▏",
-    char = "┊",
-    -- char = "│",
-    filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-    show_trailing_blankline_indent = false,
-    show_current_context = false,
+    indent = {
+      char = { "┊" },
+      -- char = "▏",
+      -- char = "│",
+    },
+    scope = {
+      show_start = false,
+      show_end = false,
+      -- highlight = { "IndentBlanklineContextChar" },
+    },
+    exclude = {
+      filetypes = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "lazy",
+      },
+    },
+    -- show_trailing_blankline_indent = false,
+    -- show_current_context = false,
   },
 }
