a3fb25b6efa136e6f7314a9005fb8793b49a4d94
Rafael Caixeta <caixetadev@gmail.com>
2024-01-16 13:34:39 +0100

    refactor: remove formatter and add conform

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,42 +1,29 @@
 return {
-  {
-    "mhartington/formatter.nvim",
-    cmd = "Format",
-    ft = { "typescript", "typescriptreact", "javascript", "javascriptreact", "html", "lua" },
-    keys = {
-      { "<leader>F", "<cmd>Format<CR>", desc = "Formatter" },
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      sh = { "beautysh" },
+      bash = { "beautysh" },
+      rust = { "rustfmt" },
+      c = { "clang_format" },
+      cpp = { "clang_format" },
+      java = { "clang_format" },
+      lua = { "stylua" },
+      go = { "goimports", "gofumpt" },
+      html = { "prettierd" },
+      javascript = { "prettierd" },
+      typescript = { "prettierd" },
+      javascriptreact = { "prettierd" },
+      typescriptreact = { "prettierd" },
+      markdown = { "prettierd" },
+      xml = { "xmlformat" },
+      json = { "prettierd" },
+      python = { "black" },
+      yaml = { "prettierd" },
     },
-    config = function()
-      require("formatter").setup({
-        logging = false,
-        filetype = {
-          lua = {
-            require("formatter.filetypes.lua").stylua,
-          },
-          html = {
-            require("formatter.filetypes.html").prettierd,
-          },
-          typescript = {
-            require("formatter.filetypes.typescript").prettierd,
-          },
-          typescriptreact = {
-            require("formatter.filetypes.typescriptreact").prettierd,
-          },
-          javascript = {
-            require("formatter.filetypes.javascript").prettierd,
-          },
-          javascriptreact = {
-            require("formatter.filetypes.javascriptreact").prettierd,
-          },
-        },
-      })
-
-      vim.cmd([[
-        augroup FormatAutogroup
-        autocmd!
-        autocmd BufWritePost * FormatWrite
-        augroup END
-      ]])
-    end,
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
   },
 }
