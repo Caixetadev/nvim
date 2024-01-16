7e631e1d7f57201858409410b508a538c43343b5
Rafael Caixeta <caixetadev@gmail.com>
2024-01-16 13:36:21 +0100

    feat: add sonarlint to golang

---
+++ lua/plugins/sonarlint.lua
@@ -0,0 +1,18 @@
+return {
+  url = "https://gitlab.com/schrieveslaach/sonarlint.nvim",
+  config = function()
+    require("sonarlint").setup({
+      server = {
+        cmd = {
+          "sonarlint-language-server",
+          "-stdio",
+          "-analyzers",
+          vim.fn.expand("$MASON/share/sonarlint-analyzers/sonargo.jar"),
+        },
+      },
+      filetypes = {
+        "go",
+      },
+    })
+  end,
+}
