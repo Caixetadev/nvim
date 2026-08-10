9e44d8cef3d1e602a777d6fd131da5866d5079d5
Rafael Caixeta <caixetadev@gmail.com>
2026-08-10 18:32:09 +0200

    chore: update telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,20 +1,20 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  version = '0.1.4',
-  dependencies = { { 'nvim-lua/plenary.nvim' } },
+  "nvim-telescope/telescope.nvim",
+  version = "*",
+  dependencies = { { "nvim-lua/plenary.nvim" } },
   keys = {
-    { "<leader>ff", "<cmd>Telescope git_files<CR>",  desc = "Find files" },
-    { "<C-p>",      "<cmd>Telescope find_files<CR>", desc = "List buffers" },
-    { "<leader>fg", "<cmd>Telescope live_grep<CR>",  desc = "Fast string grep" },
-    { "<leader>fb", "<cmd>Telescope buffers<CR>",    desc = "Recent files" },
-    { "<leader>fe", "<cmd>Telescope diagnostics",    desc = "LSP incoming calls" },
-    { "<leader>fr", "<cmd>Telescope resume<CR>",     desc = "Telescope resume" },
+    { "<leader>ff", "<cmd>Telescope git_files<CR>", desc = "Find files" },
+    { "<C-p>", "<cmd>Telescope find_files<CR>", desc = "List buffers" },
+    { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Fast string grep" },
+    { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Recent files" },
+    { "<leader>fe", "<cmd>Telescope diagnostics", desc = "LSP incoming calls" },
+    { "<leader>fr", "<cmd>Telescope resume<CR>", desc = "Telescope resume" },
     {
       "<leader>tg",
       function()
         require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
       end,
-      desc = "Grep through files"
+      desc = "Grep through files",
     },
   },
 }
